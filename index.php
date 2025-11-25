96c2247a2aca091af948bbacdb20635b1a2b123a
CassandraRabelais <leclerc.cassandra09@gmail.com>
2025-11-25 15:11:26 +0100

    Projet 1

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Page d'acceuil</title>
+<html>
+</head>
+<body>
+	La bibliothèque de Moulinsart est fermée au public jusqu'à nouvel ordre. Mais il vous est possible de réserver et retirer vos livres via notre service Biblio Drive !
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-9">
+			<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+				<form class="form-inline" action="/action_page.php">
+					<input class="form-control mr-sm-2" type="text" placeholder="Search" size="40">
+					<button class="btn btn-success" type="submit">Search</button>
+				</form>
+			</nav><br>			
+		</div>
+
+		<div class="col-md-3">
+
+		</div>
+	</div>
+	<div class="row">
+		 <div class="col-md-9">
+					
+		</div>
+		<div class="col-md-3">
+					
+		</div>
+	</div>
+</div>
+</body>
+</html>
+	
